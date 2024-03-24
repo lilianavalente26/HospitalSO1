a431245acef5568dda5025979fcaa45288a3d820
Afonso Santos <afonso.santos359@gmail.com>
2024-03-24 20:10:37 +0000

    Update methods

--- src/doctor.c
+++ src/doctor.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <doctor.h>
 
+int execute_doctor(int doctor_id, struct data_container* data, struct communication* comm){
+    
+}
+
+void doctor_receive_admission(struct admission* ad, int doctor_id, struct data_container* data, struct communication* comm){
+
+}
+
 void doctor_process_admission(struct admission* ad, int doctor_id, struct data_container* data){
     
 }
---
+++ src/test.c
@@ -0,0 +1,8 @@
+#include <memory.h>
+#include <stdio.h>
+
+int main() {
+    struct pointer* ptr;
+    *ptr.in = 10;
+    *ptr.out
+}
