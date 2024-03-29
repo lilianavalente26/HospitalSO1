01b6f0737594532fc7e580f667ca14b7cb950be8
Afonso Santos <afonso.santos359@gmail.com>
2024-03-29 17:05:00 +0000

    1ºImp: doctor_process_admission

--- src/doctor.c
+++ src/doctor.c
@@ -18,5 +18,23 @@ void doctor_receive_admission(struct admission* ad, int doctor_id, struct data_c
 }
 
 void doctor_process_admission(struct admission* ad, int doctor_id, struct data_container* data){
+    //AlteraCAo do receiving_doctor
+    ad->receiving_doctor = doctor_id;
     
+    //VariAveis
+    int *docStats = data->doctor_stats;
+    char *adStatus = ad->status;
+
+    //Verifica se existe espaCo para a admission
+    //Caso tenha espaCo
+    if (docStats < data->max_ads) {
+        *docStats++;
+        *adStatus = 'A';
+    }
+    //Caso nAo tenha espaCo
+    else {
+        *adStatus = 'N';
+    }
+    //Atualizar a admission no data
+    data->results = ad;
 }
