6c2ceb180cb968846b512bc3c5806a2ecc817371
fc59858 <madalenafmachado@gmail.com>
2024-03-30 11:28:30 +0000

    execute_doctor alterado

--- src/doctor.c
+++ src/doctor.c
@@ -2,15 +2,13 @@
 #include <doctor.h>
 
 int execute_doctor(int doctor_id, struct data_container* data, struct communication* comm){
-    int consultas;
-    while (comm->receptionist_doctor > 0){
-        if(data->terminate == 0 && comm->receptionist_doctor->buffer->id!=-1){
-            doctor_receive_admission(comm->receptionist_doctor, doctor_id, data,comm);
-            doctor_process_admission(comm->receptionist_doctor, doctor_id, data);
+    while (data->terminate == 0){
+        doctor_receive_admission(comm->receptionist_doctor->buffer, doctor_id, data,comm);
+        if(comm->receptionist_doctor->buffer->id!=-1){
+            doctor_process_admission(comm->receptionist_doctor->buffer, doctor_id, data);
         }
     }
-    consultas = sizeof(data->results);
-    return consultas;
+    return data->doctor_stats; //numero de consultas realizadas
 }
 
 void doctor_receive_admission(struct admission* ad, int doctor_id, struct data_container* data, struct communication* comm){
