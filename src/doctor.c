e70367a967fcb9bb7e4b129ce346f39ab7830f2c
fc59858 <madalenafmachado@gmail.com>
2024-03-30 11:06:43 +0000

    execute_doctor changes

--- src/doctor.c
+++ src/doctor.c
@@ -5,12 +5,12 @@ int execute_doctor(int doctor_id, struct data_container* data, struct communicat
     int consultas;
     while (comm->receptionist_doctor > 0){
         if(data->terminate == 0 && comm->receptionist_doctor->buffer->id!=-1){
+            doctor_receive_admission(comm->receptionist_doctor, doctor_id, data,comm);
             doctor_process_admission(comm->receptionist_doctor, doctor_id, data);
         }
-        else if (data->terminate == 1){
-            return sizeof(comm->receptionist_doctor);
-        }
     }
+    consultas = sizeof(data->results);
+    return consultas;
 }
 
 void doctor_receive_admission(struct admission* ad, int doctor_id, struct data_container* data, struct communication* comm){
