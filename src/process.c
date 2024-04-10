95c15d3ba5ba699e553341c40137c8af3cbb3611
Afonso Santos <afonso.santos359@gmail.com>
2024-04-10 23:30:21 +0100

    First Imp. process.c

--- src/process.c
+++ src/process.c
@@ -1,13 +1,31 @@
 #include "memory.h"
 #include "main.h"
 #include "process.h"
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
 
 /* Função que inicia um novo processo Paciente através da função fork do SO. O novo
 * processo irá executar a função execute_patient respetiva, fazendo exit do retorno.
 * O processo pai devolve o pid do processo criado.
 */
 int launch_patient(int patient_id, struct data_container* data, struct communication* comm){
-    // TODO
+    int pid = fork();
+
+    if (pid == -1) { /* Houve algum erro */
+        perror("fodeu");
+        exit(1);
+    }
+
+    if (pid == 0) { /* Processo filho */
+        execute_patient(patient_id, data, comm);
+        exit(0);
+    }
+
+    else { /* Processo pai*/
+        return pid;
+    }
 }
 
 /* Função que inicia um novo processo Rececionista através da função fork do SO. O novo
@@ -15,7 +33,21 @@ int launch_patient(int patient_id, struct data_container* data, struct communica
 * O processo pai devolve o pid do processo criado.
 */
 int launch_receptionist(int receptionist_id, struct data_container* data, struct communication* comm){
-    // TODO
+    int pid = fork();
+
+    if (pid == -1) { /* Houve algum erro */
+        perror("fodeu");
+        exit(1);
+    }
+
+    if (pid == 0) { /* Processo filho */
+        execute_receptionist(receptionist_id, data, comm);
+        exit(0);
+    }
+
+    else { /* Processo pai*/
+        return pid;
+    }
 }
 
 /* Função que inicia um novo processo Médico através da função fork do SO. O novo
@@ -23,12 +55,40 @@ int launch_receptionist(int receptionist_id, struct data_container* data, struct
 * O processo pai devolve o pid do processo criado.
 */
 int launch_doctor(int doctor_id, struct data_container* data, struct communication* comm){
-    // TODO
+    int pid = fork();
+
+    if (pid == -1) { /* Houve algum erro */
+        perror("fodeu");
+        exit(-1);
+    }
+
+    if (pid == 0) { /* Processo filho */
+        execute_doctor(doctor_id, data, comm);
+        exit(0);
+    }
+
+    else { /* Processo pai*/
+        return pid;
+    }
 }
 
 /* Função que espera que um processo termine através da função waitpid. 
 * Devolve o retorno do processo, se este tiver terminado normalmente.
 */
 int wait_process(int process_id){
-    // TODO
+    int status;
+    pid_t processo = waitpid(process_id, &status, 0);
+    
+    if (processo = -1) { /*Houve um erro*/
+        perror("fodeu");
+        return -1;
+    }
+
+    else if (WIFEXITED(status)) { /*processo terminou normalmente*/
+        return WIFEXITED(status);
+    }
+
+    else { /*erro na terminação*/
+        return -1;
+    }
 }
