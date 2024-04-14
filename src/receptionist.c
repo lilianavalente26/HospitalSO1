6767e944f8ae9c61c651e5f71ae9472d0ba83e2a
Madalena <152717819+mads-on-code@users.noreply.github.com>
2024-04-14 23:33:07 +0100

    Update receptionist.c

--- src/receptionist.c
+++ src/receptionist.c
@@ -11,15 +11,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-/* Função principal de um Rececionista. Deve executar um ciclo infinito onde em 
-* cada iteração lê uma admissão dos pacientes e se a mesma tiver id 
-* diferente de -1 e se data->terminate ainda for igual a 0, processa-a e
-* a encaminha para os médicos. Admissões com id igual a -1 são 
-* ignoradas (ad inválida) e se data->terminate for igual a 1 é porque foi 
-* dada ordem de terminação do programa, portanto deve-se fazer return do
-* número de admissões realizadas. Para efetuar estes passos, pode usar os
-* outros métodos auxiliares definidos em receptionist.h.
-*/
 int execute_receptionist(int receptionist_id, struct data_container* data, struct communication* comm){
     while (data->terminate == 0){
         if(comm->patient_receptionist->buffer->id!=-1){
@@ -28,10 +19,7 @@ int execute_receptionist(int receptionist_id, struct data_container* data, struc
     }
     return *data->receptionist_stats; //numero de admissoes realizadas
 }
-/* Função que lê uma admissão do buffer de memória partilhada entre os pacientes e os rececionistas.
-* Antes de tentar ler a admissão, deve verificar se data->terminate tem valor 1.
-* Em caso afirmativo, retorna imediatamente da função.
-*/
+
 void receptionist_receive_admission(struct admission* ad, struct data_container* data, struct communication* comm){
     //Caso data->terminate for 1 
     if (*data->terminate == 1) {
@@ -47,11 +35,6 @@ void receptionist_receive_admission(struct admission* ad, struct data_container*
     }
 }
 
-/* Função que realiza uma admissão, alterando o seu campo receiving_receptionist para o id
-* passado como argumento, alterando o estado da mesma para 'R' (receptionist), e 
-* incrementando o contador de admissões realizadas por este rececionista no data_container. 
-* Atualiza também a admissão na estrutura data.
-*/
 void receptionist_process_admission(struct admission* ad, int receptionist_id, struct data_container* data){
     //Alterar os dados
     ad->receiving_receptionist = receptionist_id;
@@ -62,9 +45,6 @@ void receptionist_process_admission(struct admission* ad, int receptionist_id, s
     data->results = ad;
 }
 
-/* Função que escreve uma admissão no buffer de memória partilhada entre
-* os rececionistas e os médicos.
-*/
 void receptionist_send_admission(struct admission* ad, struct data_container* data, struct communication* comm){
     write_receptionist_doctor_buffer(comm->receptionist_doctor, data->buffers_size, ad);
 }
