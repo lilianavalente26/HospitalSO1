fe2e4365ad7b5a49e5b0a518d927071afa5d0e73
Madalena <152717819+mads-on-code@users.noreply.github.com>
2024-04-14 23:30:49 +0100

    Update patient.c

--- src/patient.c
+++ src/patient.c
@@ -10,14 +10,6 @@
 #include "patient.h"
 
 
-/* Função principal de um Paciente. Deve executar um ciclo infinito onde em 
-* cada iteração lê uma admissão da main e se data->terminate ainda 
-* for igual a 0, processa-a. Admissões com id igual a -1 são ignoradas
-* (ad inválida) e se data->terminate for igual a 1 é porque foi dada ordem
-* de terminação do programa, portanto deve-se fazer return do número de 
-* admissões pedidas. Para efetuar estes passos, pode usar os outros
-* métodos auxiliares definidos em patient.h.
-*/
 int execute_patient(int patient_id, struct data_container* data, struct communication* comm){
     while (*data->terminate == 0){
         if(comm->main_patient->buffer->id!=-1){
@@ -26,10 +18,7 @@ int execute_patient(int patient_id, struct data_container* data, struct communic
     }
     return *data->patient_stats; //numero de admissoes pedidas
 }
-/* Função que lê uma admissão (do buffer de memória partilhada entre a main
-* e os pacientes) que seja direcionada a patient_id. Antes de tentar ler a admissão, deve
-* verificar se data->terminate tem valor 1. Em caso afirmativo, retorna imediatamente da função.
-*/
+
 void patient_receive_admission(struct admission* ad, int patient_id, struct data_container* data, struct communication* comm){
     if (*data->terminate == 1) {
         return; 
@@ -39,11 +28,6 @@ void patient_receive_admission(struct admission* ad, int patient_id, struct data
     }
 }
 
-/* Função que valida uma admissão, alterando o seu campo receiving_patient para o patient_id
-* passado como argumento, alterando o estado da mesma para 'P' (patient), e 
-* incrementando o contador de admissões solicitadas por este paciente no data_container. 
-* Atualiza também a admissão na estrutura data.
-*/
 void patient_process_admission(struct admission* ad, int patient_id, struct data_container* data){
     //Alterar os dados
     ad->receiving_patient = patient_id;
@@ -54,9 +38,6 @@ void patient_process_admission(struct admission* ad, int patient_id, struct data
     data->results = ad;
 }
 
-/* Função que escreve uma admissão no buffer de memória partilhada entre os
-* pacientes e os rececionistas.
-*/
 void patient_send_admission(struct admission* ad, struct data_container* data, struct communication* comm){
     write_patient_receptionist_buffer(comm->patient_receptionist, data->buffers_size, ad);
 }
