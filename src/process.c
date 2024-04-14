893f7a68fc277e9c8be9c378497477bffdeff0a2
Madalena <152717819+mads-on-code@users.noreply.github.com>
2024-04-14 23:32:34 +0100

    Update process.c

--- src/process.c
+++ src/process.c
@@ -17,10 +17,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Função que inicia um novo processo Paciente através da função fork do SO. O novo
-* processo irá executar a função execute_patient respetiva, fazendo exit do retorno.
-* O processo pai devolve o pid do processo criado.
-*/
 int launch_patient(int patient_id, struct data_container* data, struct communication* comm){
     int pid = fork();
 
@@ -39,10 +35,6 @@ int launch_patient(int patient_id, struct data_container* data, struct communica
     }
 }
 
-/* Função que inicia um novo processo Rececionista através da função fork do SO. O novo
-* processo irá executar a função execute_receptionist, fazendo exit do retorno.
-* O processo pai devolve o pid do processo criado.
-*/
 int launch_receptionist(int receptionist_id, struct data_container* data, struct communication* comm){
     int pid = fork();
 
@@ -61,10 +53,6 @@ int launch_receptionist(int receptionist_id, struct data_container* data, struct
     }
 }
 
-/* Função que inicia um novo processo Médico através da função fork do SO. O novo
-* processo irá executar a função execute_doctor, fazendo exit do retorno.
-* O processo pai devolve o pid do processo criado.
-*/
 int launch_doctor(int doctor_id, struct data_container* data, struct communication* comm){
     int pid = fork();
 
@@ -83,9 +71,6 @@ int launch_doctor(int doctor_id, struct data_container* data, struct communicati
     }
 }
 
-/* Função que espera que um processo termine através da função waitpid. 
-* Devolve o retorno do processo, se este tiver terminado normalmente.
-*/
 int wait_process(int process_id){
     int status;
     pid_t processo = waitpid(process_id, &status, 0);
